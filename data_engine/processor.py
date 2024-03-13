fb7fe1a7dc63f0abda3cf5d8359880c1a00706fb
f1Analytics <f1.analytics.info@gmail.com>
2024-03-13 22:45:22 +0100

    feat: :sparkles: added quali comparison

--- data_engine/processor.py
+++ data_engine/processor.py
@@ -1,6 +1,9 @@
 import fastf1 as ff1
+from fastf1 import plotting
+from fastf1 import utils
+
 import matplotlib.pyplot as plt
-from .main import GPBucket
+from main import GPBucket
 class Processor:
     def __init__(self, bucket):
         self.bucket: GPBucket = bucket
@@ -50,4 +53,84 @@ class Processor:
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
         ax.spines['left'].set_visible(False)
+        return fig
+    
+    def create_best_lap_comparison_summary(self, driver_1: str=None, driver_2: str=None):
+        quali = self.bucket.quali
+        laps_driver_1 = quali.laps.pick_driver(driver_1)
+        laps_driver_2 = quali.laps.pick_driver(driver_2)
+        
+        # Select the fastest lap
+        fastest_driver_1 = laps_driver_1.pick_fastest()
+        fastest_driver_2 = laps_driver_2.pick_fastest()
+                
+        # Retrieve the telemetry and add the distance column
+        telemetry_driver_1 = fastest_driver_1.get_telemetry().add_distance()
+        telemetry_driver_2 = fastest_driver_2.get_telemetry().add_distance()
+
+        # Make sure whe know the team name for coloring
+        team_driver_1 = fastest_driver_1['Team']
+        team_driver_2 = fastest_driver_2['Team']
+       
+        # Extract the delta time
+        delta_time, ref_tel, compare_tel = utils.delta_time(fastest_driver_1, fastest_driver_2)
+
+        plot_size = [15, 15]
+        plot_title = f"{quali.event.year} {quali.event.EventName} - {quali.name} - {driver_1} vs {driver_2}"
+        plot_ratios = [1, 4, 2, 1, 2]
+        plot_filename = plot_title.replace(" ", "_") + ".png"
+
+        # Make plot a bit bigger
+        plt.rcParams['figure.figsize'] = plot_size
+
+        # Create subplots with different sizes
+        fig, ax = plt.subplots(5, gridspec_kw={'height_ratios': plot_ratios})
+
+        # Set the plot title
+        ax[0].set_title(plot_title, fontsize=25)
+
+
+        # Delta line
+        ax[0].plot(ref_tel['Distance'], delta_time)
+        ax[0].axhline(0)
+        ax[0].set(ylabel=f"Gap to {driver_2} (s)")
+
+        # Speed trace
+        ax[1].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Speed'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
+        ax[1].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Speed'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
+        ax[1].set(ylabel='Speed')
+        ax[1].legend(loc="lower right")
+
+        # Throttle trace
+        ax[2].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Throttle'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
+        ax[2].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Throttle'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
+        ax[2].set(ylabel='Throttle')
+
+        # # Brake trace
+        # ax[3].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Brake'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
+        # ax[3].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Brake'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
+        # ax[3].set(ylabel='Brake')
+
+        # Gear trace
+        ax[3].plot(telemetry_driver_1['Distance'], telemetry_driver_1['nGear'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
+        ax[3].plot(telemetry_driver_2['Distance'], telemetry_driver_2['nGear'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
+        ax[3].set(ylabel='Gear')
+
+        # RPM trace
+        ax[4].plot(telemetry_driver_1['Distance'], telemetry_driver_1['RPM'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
+        ax[4].plot(telemetry_driver_2['Distance'], telemetry_driver_2['RPM'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
+        ax[4].set(ylabel='RPM')
+
+
+        # # DRS trace
+        # ax[6].plot(telemetry_driver_1['Distance'], telemetry_driver_1['DRS'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
+        # ax[6].plot(telemetry_driver_2['Distance'], telemetry_driver_2['DRS'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
+        # ax[6].set(ylabel='DRS')
+        # ax[6].set(xlabel='Lap distance (meters)')
+
+
+        # Hide x labels and tick labels for top plots and y ticks for right plots.
+        for a in ax.flat:
+            a.label_outer()
+    
         return fig
--- data_engine/utils.py
+++ data_engine/utils.py
@@ -1,2 +1,12 @@
-def get_fig_path(year: int, gp_id: int):
+import os
+import logging
+import matplotlib.pyplot as plt
+
+def get_fig_path(year: int, gp_id: int) -> str:
     return f"figures/{year}/{gp_id}/"
+
+def save_plot(fig, year: int, gp_id: int, summary_filename: str) -> None:
+    filepath = get_fig_path(year, gp_id)
+    os.makedirs(filepath, exist_ok=True)
+    fig.savefig(get_fig_path(year, gp_id) + summary_filename + ".png")
+    logging.info(f"Figure saved at {filepath}tyre_summary.png")
