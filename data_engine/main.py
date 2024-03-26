8dc1c0503b2f1682028a5fa09f862048d73e5c0d
f1Analytics <f1.analytics.info@gmail.com>
2024-03-26 21:29:14 +0100

    style(ETL pipeline): :art: formatting

--- data_engine/main.py
+++ data_engine/main.py
@@ -4,74 +4,81 @@ import matplotlib.pyplot as plt
 
 import fastf1 as ff1
 import fastf1.plotting
+
 fastf1.plotting.setup_mpl(misc_mpl_mods=False)
 
 directory_of_this_file = Path(__file__).parent
-ff1.Cache.enable_cache(directory_of_this_file/'cache/') 
+ff1.Cache.enable_cache(directory_of_this_file / "cache/")
+
 
 class EngineBucket:
     _COMPOUND_COLORS = {
-        'SOFT': '#FF3333',
-        'MEDIUM': '#FFF200',
-        'HARD': '#EBEBEB',
-        'INTERMEDIATE': '#39B54A',
-        'WET': '#00AEEF',
-        'UNKNOWN': '#000000'}
-    
+        "SOFT": "#FF3333",
+        "MEDIUM": "#FFF200",
+        "HARD": "#EBEBEB",
+        "INTERMEDIATE": "#39B54A",
+        "WET": "#00AEEF",
+        "UNKNOWN": "#000000",
+    }
+
     def __init__(self, gp_id, year):
         self.gp_id = gp_id
         self.year = year
         self.fetcher = Fetcher()
-        
+
+
 class GPBucket(EngineBucket):
     def __init__(self, gp_id, year):
         super().__init__(gp_id, year)
         self.quali = None
         self.race = None
-        
+
         self._processor = None
-        
+
         self._set_gp_info()
         self._set_static_data()
-        
+
         # processing data
-        self.race_laps = None   
-    
+        self.race_laps = None
+
     @property
     def processor(self):
         if self._processor is None:
             from processor import Processor
+
             self._processor = Processor(bucket=self)
         return self._processor
-    
+
     def _set_static_data(self):
         self.data = self.fetcher.fetch_static_data()
         self.gp_name = ""
-    
+
     def _set_gp_info(self):
-        self.quali = ff1.get_session(self.year, self.gp_id, 'Q')
+        ff1.get_session()
+        self.quali = ff1.get_session(self.year, self.gp_id, "Q")
         self.quali.load()
-        self.race = ff1.get_session(self.year, self.gp_id, 'R')
+        self.race = ff1.get_session(self.year, self.gp_id, "R")
         self.race.load()
-        
+
     def get_tyre_strategy_summary(self):
         fig = self.processor.create_tyre_strategy_summary()
         return fig
-    
+
     def get_quali_comparison(self, driver_1, driver_2):
         fig = self.processor.create_best_lap_comparison_summary(driver_1, driver_2)
         return fig
-    
-    def get_race_data(self, year:int=None, gp_id:int=None):
+
+    def get_race_data(self, year: int = None, gp_id: int = None):
         return self.race
-        
+
     def get_race_laps_data(self, race=None):
         if self.race_laps is None:
             if race is None:
                 race = self.race
             self.race_laps = race.laps
         return self.race_laps
-    
+
+
 if __name__ == "__main__":
     bucket = GPBucket(year=2024, gp_id=2)
     fig = bucket.get_quali_comparison("VER", "LEC")
--- data_engine/processor.py
+++ data_engine/processor.py
@@ -4,23 +4,27 @@ from fastf1 import utils
 
 import matplotlib.pyplot as plt
 from main import GPBucket
+
+
 class Processor:
     def __init__(self, bucket):
         self.bucket: GPBucket = bucket
-    
-    def create_tyre_strategy_summary(self):
 
+    def create_tyre_strategy_summary(self):
         race = self.bucket.get_race_data()
         laps_data = race.laps
         drivers = race.drivers
         drivers = [race.get_driver(driver)["Abbreviation"] for driver in drivers]
-        
-        driver_stints = laps_data[['Driver', 'Stint', 'Compound', 'LapNumber']].groupby(
-            ['Driver', 'Stint', 'Compound']
-        ).count().reset_index()
 
-        driver_stints = driver_stints.rename(columns={'LapNumber': 'StintLength'})
-        driver_stints = driver_stints.sort_values(by=['Stint'])
+        driver_stints = (
+            laps_data[["Driver", "Stint", "Compound", "LapNumber"]]
+            .groupby(["Driver", "Stint", "Compound"])
+            .count()
+            .reset_index()
+        )
+
+        driver_stints = driver_stints.rename(columns={"LapNumber": "StintLength"})
+        driver_stints = driver_stints.sort_values(by=["Stint"])
 
         plt.rcParams["figure.figsize"] = [15, 10]
         plt.rcParams["figure.autolayout"] = True
@@ -28,8 +32,8 @@ class Processor:
         fig, ax = plt.subplots()
 
         for driver in drivers:
-            stints = driver_stints.loc[driver_stints['Driver'] == driver]
-            
+            stints = driver_stints.loc[driver_stints["Driver"] == driver]
+
             previous_stint_end = 0
             for _, stint in stints.iterrows():
                 plt.barh(
@@ -38,42 +42,46 @@ class Processor:
                     left=previous_stint_end,
                     color=self.bucket._COMPOUND_COLORS[stint["Compound"]],
                     edgecolor="black",
-                    fill=True
+                    fill=True,
                 )
 
-                previous_stint_end += stint["StintLength"]              
-        
-        plt.title(f'Tyre strategy - {self.bucket.year} {self.bucket.gp_id}')
+                previous_stint_end += stint["StintLength"]
+
+        plt.title(f"Tyre strategy - {self.bucket.year} {self.bucket.gp_id}")
         plt.xlabel("Lap Number")
         plt.grid(False)
         # invert the y-axis so drivers that finish higher are closer to the top
         ax.invert_yaxis()
 
         # Remove frame from plot
-        ax.spines['top'].set_visible(False)
-        ax.spines['right'].set_visible(False)
-        ax.spines['left'].set_visible(False)
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.spines["left"].set_visible(False)
         return fig
-    
-    def create_best_lap_comparison_summary(self, driver_1: str=None, driver_2: str=None):
+
+    def create_best_lap_comparison_summary(
+        self, driver_1: str = None, driver_2: str = None
+    ):
         quali = self.bucket.quali
         laps_driver_1 = quali.laps.pick_driver(driver_1)
         laps_driver_2 = quali.laps.pick_driver(driver_2)
-        
+
         # Select the fastest lap
         fastest_driver_1 = laps_driver_1.pick_fastest()
         fastest_driver_2 = laps_driver_2.pick_fastest()
-                
+
         # Retrieve the telemetry and add the distance column
         telemetry_driver_1 = fastest_driver_1.get_telemetry().add_distance()
         telemetry_driver_2 = fastest_driver_2.get_telemetry().add_distance()
 
         # Make sure whe know the team name for coloring
-        team_driver_1 = fastest_driver_1['Team']
-        team_driver_2 = fastest_driver_2['Team']
-       
+        team_driver_1 = fastest_driver_1["Team"]
+        team_driver_2 = fastest_driver_2["Team"]
+
         # Extract the delta time
-        delta_time, ref_tel, compare_tel = utils.delta_time(fastest_driver_1, fastest_driver_2)
+        delta_time, ref_tel, compare_tel = utils.delta_time(
+            fastest_driver_1, fastest_driver_2
+        )
 
         plot_size = [15, 15]
         plot_title = f"{quali.event.year} {quali.event.EventName} - {quali.name} - {driver_1} vs {driver_2}"
@@ -81,30 +89,49 @@ class Processor:
         plot_filename = plot_title.replace(" ", "_") + ".png"
 
         # Make plot a bit bigger
-        plt.rcParams['figure.figsize'] = plot_size
+        plt.rcParams["figure.figsize"] = plot_size
 
         # Create subplots with different sizes
-        fig, ax = plt.subplots(5, gridspec_kw={'height_ratios': plot_ratios})
+        fig, ax = plt.subplots(5, gridspec_kw={"height_ratios": plot_ratios})
 
         # Set the plot title
         ax[0].set_title(plot_title, fontsize=25)
 
-
         # Delta line
-        ax[0].plot(ref_tel['Distance'], delta_time)
+        ax[0].plot(ref_tel["Distance"], delta_time)
         ax[0].axhline(0)
         ax[0].set(ylabel=f"Gap to {driver_2} (s)")
 
         # Speed trace
-        ax[1].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Speed'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
-        ax[1].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Speed'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
-        ax[1].set(ylabel='Speed')
+        ax[1].plot(
+            telemetry_driver_1["Distance"],
+            telemetry_driver_1["Speed"],
+            label=driver_1,
+            color=ff1.plotting.team_color(team_driver_1),
+        )
+        ax[1].plot(
+            telemetry_driver_2["Distance"],
+            telemetry_driver_2["Speed"],
+            label=driver_2,
+            color=ff1.plotting.team_color(team_driver_2),
+        )
+        ax[1].set(ylabel="Speed")
         ax[1].legend(loc="lower right")
 
         # Throttle trace
-        ax[2].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Throttle'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
-        ax[2].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Throttle'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
-        ax[2].set(ylabel='Throttle')
+        ax[2].plot(
+            telemetry_driver_1["Distance"],
+            telemetry_driver_1["Throttle"],
+            label=driver_1,
+            color=ff1.plotting.team_color(team_driver_1),
+        )
+        ax[2].plot(
+            telemetry_driver_2["Distance"],
+            telemetry_driver_2["Throttle"],
+            label=driver_2,
+            color=ff1.plotting.team_color(team_driver_2),
+        )
+        ax[2].set(ylabel="Throttle")
 
         # # Brake trace
         # ax[3].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Brake'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
@@ -112,15 +139,34 @@ class Processor:
         # ax[3].set(ylabel='Brake')
 
         # Gear trace
-        ax[3].plot(telemetry_driver_1['Distance'], telemetry_driver_1['nGear'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
-        ax[3].plot(telemetry_driver_2['Distance'], telemetry_driver_2['nGear'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
-        ax[3].set(ylabel='Gear')
+        ax[3].plot(
+            telemetry_driver_1["Distance"],
+            telemetry_driver_1["nGear"],
+            label=driver_1,
+            color=ff1.plotting.team_color(team_driver_1),
+        )
+        ax[3].plot(
+            telemetry_driver_2["Distance"],
+            telemetry_driver_2["nGear"],
+            label=driver_2,
+            color=ff1.plotting.team_color(team_driver_2),
+        )
+        ax[3].set(ylabel="Gear")
 
         # RPM trace
-        ax[4].plot(telemetry_driver_1['Distance'], telemetry_driver_1['RPM'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
-        ax[4].plot(telemetry_driver_2['Distance'], telemetry_driver_2['RPM'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
-        ax[4].set(ylabel='RPM')
-
+        ax[4].plot(
+            telemetry_driver_1["Distance"],
+            telemetry_driver_1["RPM"],
+            label=driver_1,
+            color=ff1.plotting.team_color(team_driver_1),
+        )
+        ax[4].plot(
+            telemetry_driver_2["Distance"],
+            telemetry_driver_2["RPM"],
+            label=driver_2,
+            color=ff1.plotting.team_color(team_driver_2),
+        )
+        ax[4].set(ylabel="RPM")
 
         # # DRS trace
         # ax[6].plot(telemetry_driver_1['Distance'], telemetry_driver_1['DRS'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
@@ -128,9 +174,8 @@ class Processor:
         # ax[6].set(ylabel='DRS')
         # ax[6].set(xlabel='Lap distance (meters)')
 
-
         # Hide x labels and tick labels for top plots and y ticks for right plots.
         for a in ax.flat:
             a.label_outer()
-    
+
         return fig
